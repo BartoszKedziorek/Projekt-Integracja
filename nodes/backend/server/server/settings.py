18c3eddf8f31ba1fc9154d4b9114b5432bf8e5cd
BartoszKedziorek <bartoszkedziorek2014@wp.pl>
2024-06-03 16:45:33 +0200

    fix: cors headers

--- nodes/backend/server/server/settings.py
+++ nodes/backend/server/server/settings.py
@@ -44,9 +44,12 @@ INSTALLED_APPS = [
     'users',
     'rest_framework.authtoken',
     'drf_spectacular',
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
