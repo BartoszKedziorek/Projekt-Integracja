962b935c9deedcf8076b027f09c74790a9fd95fd
BartoszKedziorek <bartoszkedziorek2014@wp.pl>
2024-06-06 16:27:32 +0200

    docker: create superuser

--- nodes/backend/load_and_run.sh
+++ nodes/backend/load_and_run.sh
@@ -1,4 +1,13 @@
 python ./DatasetsPythonClient/load_countries.py;
 python ./DatasetsPythonClient/DatasetsClient.py;
 python manage.py migrate users
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
 python manage.py runserver 0.0.0.0:8000;
